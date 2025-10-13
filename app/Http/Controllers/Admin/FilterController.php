ddaae74159cfedd8d6a37ba0dcb9472c66a6c48f
dakshj31 <dakshjoshi1355@gmail.com>
2025-10-13 23:49:15 +0530

    Product Filters (VIII) Manage Filters from Admin Panel Filters CRUD

--- app/Http/Controllers/Admin/FilterController.php
+++ app/Http/Controllers/Admin/FilterController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Admin\FilterRequest;
+use App\Services\Admin\FilterServices;
+use App\Models\Filter;
+use App\Models\Category;
 
 class FilterController extends Controller
 {
+
+    protected $filterService;
+
+    public function __construct(FilterService $filterService)
+    {
+        $this->filterService = $filterService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $filters = $this->filterSevice->getAll();
+        $title = "Filters";
+        return view('admin.filters.index', compact('filters','title'));
     }
 
     /**
@@ -20,15 +33,18 @@ class FilterController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::with('subcategories')->where('parent_id', null)->get();
+        $title = "Add Filter";
+        return view('admin.filters.add_edit_filter', compact('categories','title'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(FilterRequest $request)
     {
-        //
+        $this->filterService->store($request->validated());
+        return redirect()->route('filters.index')->with('success_message', 'FIlter added successfully');
     }
 
     /**
@@ -36,30 +52,52 @@ class FilterController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $filter = FIlter::with('categories')->findOrFail($id);
+
+        $categories = Category::with('subcategories')->where('parent_id', null)->where('status', 1)->get();
+
+        $selectedCategories = $filter->categories->pluck('id')->toArray();
+
+        return view('admin.filters.add_edit_filter', [
+            'title' => 'Edit Filter',
+            'filter' => $filter,
+            'categories' -> $categories,
+            'selectedCategories' => $selectedCategories
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(FilterRequest $request, $id)
     {
-        //
+        $this->filterService->update($id, $request->validated());
+        return redirect()-route('filters.index')->with('success_message', 'Filter updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $this->filterService->delete($id);
+        return redirect()->route('filters.index')->with('success_message', 'Filter deleted successfully');
+    }
+
+    public function updateFilterStatus(Request $request)
+    {
+        if($request->ajax()) {
+            $data = $request->all();
+            $status = $this->filterSevice->updateFilterStatus($data);
+            return response()->json(['status' => $status, 'filter_id' => $data['filter_id']]);
+        }
     }
 }
